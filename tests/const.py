e2c9f42f618f9d6fc1ccf5e00641c52449d8a6c2
Kent Husvik <kent.husvik@gmail.com>
2026-02-05 12:03:37 +0100

    Clean up repository - remove dev files
    - Remove hass dev script
    - Remove tests/ (outdated)
    - Remove .devcontainer/ and .vscode/ (dev configs)
    - Remove .pylintrc, setup.cfg, requirements_dev/test.txt
    - Remove todo.md
    - Update .gitignore

--- tests/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for openHAB tests."""
-from custom_components.openhab.const import CONF_BASE_URL, CONF_PASSWORD, CONF_USERNAME
-
-# Mock config data to be used across multiple tests
-MOCK_CONFIG = {
-    CONF_BASE_URL: "http://openhab:8080",
-    CONF_USERNAME: "test_username",
-    CONF_PASSWORD: "test_password",
-}
